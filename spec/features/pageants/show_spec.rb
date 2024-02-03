e5031657d8968bdf035e81e1f7bbdbc7e543af3c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-02 21:52:55 -0700

    Feat: show pageants number of contestants, tests and feature for user story7

--- spec/features/pageants/show_spec.rb
+++ spec/features/pageants/show_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'Pageant Show', type: :feature do
     before(:each) do
       @pageant1 = Pageant.create!(name: "Miss Colombia", scheduled: true, version_number: 10)
       @pageant2 = Pageant.create!(name: "Miss Universe", scheduled: true, version_number: 25)
+      @contestant1 = @pageant2.contestants.create!(name: "Colombia", years_of_experience: 57, has_representative: true)
+      @contestant2 = @pageant2.contestants.create!(name: "Peru", years_of_experience: 75, has_representative: true)
     end
 
     # User Story 2, Parent Show 
@@ -14,6 +16,14 @@ RSpec.describe 'Pageant Show', type: :feature do
       expect(page).to have_content(@pageant1.name)
       expect(page).to have_content(@pageant1.scheduled)
       expect(page).to have_content(@pageant1.version_number)
+    end
+
+    # User Story 7, Parent Child Count
+    it 'can show the count of the number of contestants associated with this pageant' do
+      visit "/pageants/#{@pageant2.id}"
+
+      expected_text = "#{@pageant2.name} has #{@pageant2.contestants.count} contestants."
+      expect(page).to have_content(expected_text.strip)
     end
  end
 end
